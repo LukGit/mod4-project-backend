61aeaca4e6c8017c5d7b3c4b4a521c039c151dfd
Shau Yin Luk <ivan.luk@icloud.com>
2020-06-14 20:54:49 -0500

    backend setup

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,18 @@
 class NotesController < ApplicationController
+
+  def index
+    notes = Note.all
+    render json: notes, include: [:user] 
+  end
+
+  def create
+    note = Note.create!(note_params)
+    render json: note
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :title, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users, include: [:notes] 
+  end
+
+  def create
+    user = User.find_or_create_by!(name: params[:name])
+    render json: user, include: [:notes] 
+    # render json: UserSerializer.new(user)
+  end
+
 end
