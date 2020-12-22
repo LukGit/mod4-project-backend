1bb4277db0a6da3e5da8367386d34070b81144ad
Shau Yin Luk <ivan.luk@icloud.com>
2020-12-22 14:59:37 -0600

    user params used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_or_create_by!(name: params[:name])
+    # user = User.find_or_create_by!(name: params[:name])
+    user = User.find_or_create_by!(user_params)
     render json: user, include: [:notes] 
     # render json: UserSerializer.new(user)
+  end
+
+  private
+
+  def user_params
+    params.require(:name)
   end
 
 end
